9e3004a4f0e6b309634dfa9c86e851e75b70e8de
radhika mendiratta <radhikamendiratta1994@gmail.com>
2023-05-13 11:51:15 +0530

    setup: firebase config files setup

---
+++ crypto-coin-com/src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA9NMMCTWEjVL217CYonypNQiV1ray7PWg",
+  authDomain: "crypto-coin-8feed.firebaseapp.com",
+  projectId: "crypto-coin-8feed",
+  storageBucket: "crypto-coin-8feed.appspot.com",
+  messagingSenderId: "275680920393",
+  appId: "1:275680920393:web:c46d3eb76b954f89d35098",
+};
+
+export default firebaseConfig;
